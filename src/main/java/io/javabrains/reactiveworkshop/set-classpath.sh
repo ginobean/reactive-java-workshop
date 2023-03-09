ddbbfe73ffd7bc9318bfab44c0757132fcb7864f
Gino Lee <ginolee859@gmail.com>
2023-03-09 12:16:48 -0500

    add some notes on how to run these sample programs

---
+++ src/main/java/io/javabrains/reactiveworkshop/set-classpath.sh
@@ -0,0 +1,4 @@
+# note to self:
+# You need to source this file, in order for the environment variable to
+# be set in your current shell environment:
+export CLASSPATH=/nfs/netdrive/remote/reactive-java-workshop/src/main/java/
