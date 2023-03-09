f8dafdec989b9e1506e868dae70b65c5e4f0ac30
Gino Lee <ginolee859@gmail.com>
2023-03-09 12:42:15 -0500

    did exercise 2

--- src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -12,10 +12,13 @@ public class Exercise2 {
 
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
         // TODO: Write code here
-        ReactiveSources.intNumbersFlux().subscribe(v -> System.out.println(v));
+        ReactiveSources.intNumbersFlux().subscribe(v -> System.out.printf(" %s ", v));
+        System.out.println();
 
         // Print all users in the ReactiveSources.userFlux stream
         // TODO: Write code here
+
+        ReactiveSources.userFlux().subscribe(user -> System.out.printf(" user = %s ", user));
 
         System.out.println("Press a key to end");
         System.in.read();
