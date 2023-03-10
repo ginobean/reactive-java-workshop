a39a1fb86fb517e2d0aafeb618be0689a75faca7
Gino Lee <ginolee859@gmail.com>
2023-03-10 13:08:14 -0500

    wip

--- src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
@@ -1,6 +1,8 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Exercise4 {
 
@@ -10,9 +12,18 @@ public class Exercise4 {
 
         // Print the value from intNumberMono when it emits
         // TODO: Write code here
+        ReactiveSources.intNumberMono().subscribe(v -> {
+            System.out.printf("mono result = %s\n", v);
+        });
 
         // Get the value from the Mono into an integer variable
         // TODO: Write code here
+        List<Integer> results = new ArrayList<>();
+        ReactiveSources.intNumberMono().subscribe(v -> {
+            results.add(v);
+            System.out.printf("assigned to monoResult\n");
+            System.out.printf(" new results size = %s\n", results.size());
+        });
 
         System.out.println("Press a key to end");
         System.in.read();
